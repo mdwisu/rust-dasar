c3d7ddba7968f314c444662776d9cb0ae1e44aaf
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2025-09-13 13:50:16 +0700

    unit test and variable

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[test]
+fn hello_test() {
+    println!("Hello, test!");
+}
+
+#[test]
+fn test_variable() {
+    let name = "muhammad dwi susanto";
+    println!("Hello, {}", name);
+}
+
