10535061674b2b9e5447e15d75112d4b4f18e3b5
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2025-09-15 10:46:18 +0700

    data type, number, int, float, isize, usize, number conversion

--- src/main.rs
+++ src/main.rs
@@ -35,3 +35,33 @@ fn comment() {
     println!("Hello, comment!");
 }
 
+#[test]
+fn explicit_type() {
+    let name: &str = "dwi";
+    let age: u8 = 20;
+    let is_married: bool = false;
+    let height: f32 = 170.5;
+    println!("name: {}, age: {}, is_married: {}, height: {}", name, age, is_married, height);
+}
+
+#[test]
+fn number(){
+    let a: i32 = 10;
+    println!("a: {}", a);
+    let b: f64 = 20.5; 
+    println!("b: {}", b);
+    let c: isize = 30; 
+    println!("c: {}", c);
+    let d: usize = 40;
+    println!("d: {}", d);
+}
+
+#[test]
+fn number_conversion() {
+    let a: i32 = 10;
+    let b: f64 = a as f64;
+    println!("b: {}", b);
+    let c: u8 = 20;
+    let d: i32 = c as i32;
+    println!("d: {}", d);
+}
